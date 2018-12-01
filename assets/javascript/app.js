a071e5bf848233c6be3aff3934e14ad02a1aa7e6
Stan Bakalov <sunny.bakalov@gmail.com>
2018-12-01 15:23:27 -0700

    added buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
-var apiKey = "&api_key=VuemM9qnf1AxAVeW5yvUVDSa1KQACfl3";
+var apiKey = "&api_key=VuemM9qnf1AxAVeW5yvUVDSa1KQACfl3&limit=10";
 var url = "http://api.giphy.com/v1/gifs/search?q=";
 var searchTerm = "50 cent";
 var queryUrl = url + searchTerm + apiKey;
+var number = 10;
 
 $.ajax({
     url: queryUrl,
@@ -9,4 +10,8 @@ $.ajax({
 }) .then(function(response){
     console.log(response);
     //need a for loop that will spit out 10 gifs at once
+    // for (var i = 0; i < response.data.length; i++) {
+    //     var gifDiv = $("#gifDiv");
+
+    // }
 });
