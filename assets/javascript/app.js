647ebe9aa5600bbea9a393f4060e87679a4c8425
Stan Bakalov <sunny.bakalov@gmail.com>
2018-12-01 18:03:39 -0700

    trying to toggle between the still and gif but no luck yet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,16 +14,33 @@ $("button").on("click", function() {
         method: "GET"
     }) .then(function(response){
         console.log(response);
+
+        var results = response.data
         //need a for loop that will spit out 10 gifs at once
-        for (var i = 0; i < response.data.length; i++) {
-            //created new div to store the gifs in
-            var gifDiv = $("<div>");
+        for (var i = 0; i < results.length; i++) {
 
             var image = $("<img>");
-            image.attr("src", response.data[i].images.fixed_height.url);
-
-            gifDiv.prepend(image);
-            $("#gifDiv").prepend(gifDiv);
+            // var image2 = $("<img>");
+            image.attr("src", results[i].images.fixed_height_still.url);
+            // image2.attr("src", results[i].images.fixed_height.url);
+            image.attr({'data-animate' : results[i].images.fixed_height.url});
+            image.attr({'data-state' : "still"});
+            image.attr({'data-still' : results[i].images.fixed_height_still.url});
+            $("#gifDiv").prepend(image);
         }
     });
 });
+
+$("img").on("click", function() {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+    // $(this).attr("src", $(this).attr("data-animate"));
+
+})
